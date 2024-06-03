47d1cf87c5f63d79754868dbf6c6732e4e87b9c2
NICOLÁS DAVID BECHERMAN <47700068@ort>
2024-06-03 12:06:08 -0300

    3.0

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -1,29 +1,24 @@
 import React from 'react';
 import Logo from './logo';
-import Desplegable from './desplegable';
-
+import Desplegable from './Desplegable';
 import imgLogo from '../vendor/logo.png'; 
-
 import '../css/header.css';
 
 const Header = () => {
   return (
     <header>
-      <div className='logo'><Logo destino="#Index" img={imgLogo} /></div>
-      
+      <div className='logo'>
+        <Logo destino="#Index" img={imgLogo} />
+      </div>
       <div className='desplegable'>
-      
-      <Desplegable texto="Hogar" />
-      <Desplegable texto="Educacion" />
-      <Desplegable texto="Consultoria"  />
-      <Desplegable texto="Diseño"  />
-      <Desplegable texto="Desarollo personal" />
-      
+        <Desplegable texto="Hogar" />
+        <Desplegable texto="Educación" />
+        <Desplegable texto="Consultoría" />
+        <Desplegable texto="Diseño" />
+        <Desplegable texto="Desarrollo personal" />
       </div>
     </header>
   );
 }
 
 export default Header;
-
-//
